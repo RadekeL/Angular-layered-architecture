69a73fba03798f7f4557b5a1a5af1529cb93dbe5
RadekeL <radoslawlejman@gmail.com>
2021-07-17 14:22:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-dvsfqa
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-dvsfqa)
---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { AddTasksComponent } from '../todo-app/add-tasks/add-tasks.component';
+import { CompletedTasksComponent } from '../todo-app/completed-tasks/completed-tasks.component';
+import { TodoTasksComponent } from '../todo-app/todo-tasks/todo-tasks.component';
+import { TasksApiService } from '../todo-app/tasks-api.service';
+import { TasksListComponent } from '../todo-app/tasks-list/tasks-list.component';
+import { TasksStateComponent } from '../todo-app/tasks-state.component';
+
+@NgModule({
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    AddTasksComponent,
+    CompletedTasksComponent,
+    TodoTasksComponent,
+    TasksListComponent
+  ],
+  providers: [TasksStateComponent, TasksApiService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
